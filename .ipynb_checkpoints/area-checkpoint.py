107218666e803c36f80b9a33458fc606cc848bf2
Hizb Ullah <hizbullahjan996@gmail.com>
2024-06-24 10:21:01 +0500

    Initial commit

---
+++ .ipynb_checkpoints/area-checkpoint.py
@@ -0,0 +1,14 @@
+
+def circle_area(radius):
+   radius = (3.14 * radius ** 2)
+    return radius
+
+def rectangle_area(length, width):
+    return (length * width)
+
+def triangle_area(base, height):
+    return (0.5 * base * height)
+
+
+
+
---
+++ area.py
@@ -0,0 +1,9 @@
+def circle_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+def rectangle_area(length, width):
+    return length * width
+
+def triangle_area(base, height):
+    return 0.5 * base * height
